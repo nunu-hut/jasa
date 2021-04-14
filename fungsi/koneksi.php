db37b1155723a9c04e96c565c069ca0d3822593d
nunu-hut <nurulismailiah1@gmail.com>
2021-04-14 15:24:22 +0700

    first commit

--- fungsi/koneksi.php
+++ fungsi/koneksi.php
@@ -1,7 +1,7 @@
 <?php 
  
 	$base_url = "http://localhost/jasa/";
-	$mysqli = mysqli_connect("localhost:3309","root","","jasa");
+	$mysqli = mysqli_connect("localhost","root","","jasa");
 	
 	if (mysqli_connect_errno()){
 		echo "Database tidak ditemukan." . mysqli_connect_error();
